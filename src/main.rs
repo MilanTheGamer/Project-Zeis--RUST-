aa4163b4d67570aeb2304351fcce0486ebbab29d
TypicalEngineer <milanthegamer@gmail.com>
2021-10-09 19:56:09 +0530

    Projet Initiated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Project ZEUS");
+    let server = Server::new("ZEUS".to_string());
+    server.run();
 }
+
+struct Server {
+    server_name:String
+}
+
+impl Server {
+    fn new(server_name:String) -> Self {
+        Self{
+            server_name
+        }
+    }
+
+    fn run(self) {
+        println!("{} is running",self.server_name)
+    }
+}
+
+
